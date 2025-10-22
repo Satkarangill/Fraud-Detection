7a77b99b7ce0771875e33666bbf4bc7e99df59d1
Satkaran <satkarangill0@gmail.com>
2025-10-22 00:32:53 -0400

    "changed create_db to accomodate for budget model"

--- Backend/core/create_db.py
+++ Backend/core/create_db.py
@@ -1,7 +1,9 @@
-from Database import Base, engine
-import models
+from Backend.core.database import Base, engine
+# import the actual model class names used in the model files
+from Backend.table_models.user_model import User
+from Backend.table_models.transaction_model import Transaction
+from Backend.table_models.budget_model import BudgetTable
 
 print("Creating database tables...")
 Base.metadata.create_all(bind=engine)
-print("Done!")
-
+print("Done ✅")
